58fdc475451061f712593e851c8950555388dfab
vamsikrishna <vamsimalle1790@gmail.com>
2024-12-03 20:09:35 +0530

    fourth commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import mysql.connector
+import os
 
 app = FastAPI()
 #checking web-hoo
